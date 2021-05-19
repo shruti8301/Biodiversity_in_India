a11c197f4d3a6d1c98c2e08c97e5d738887b5b18
Shruti Gupta <shruti8301@gmail.com>
2021-05-19 10:52:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
-" >~/.streamlit/credentials.toml
+" > ~/.streamlit/credentials.toml
 # shellcheck disable=SC2028
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" >~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
